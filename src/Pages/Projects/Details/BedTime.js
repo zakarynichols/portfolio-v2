2cf4fbbc9d98cbe8eb04b1b46db04c1923b40446
zaknicholsdev <zaknicholsdev1993@gmail.com>
2020-12-28 23:06:14 -0500

    writing markdown

--- src/Pages/Projects/Details/BedTime.js
+++ src/Pages/Projects/Details/BedTime.js
@@ -4,28 +4,19 @@ const BedTime = ({ name }) => {
     const width = "w"
     const markdown = `
 # ${name}
-wooo is this working.  
-skdfljsdkfj
+First things first. Let's get our sleep cycles. Create a function that returns an array of sleep cycles.  
+For each sleep cycle insantiate a new \`Date\` object. We only need the hours and minutes so we can omit  
+the first three arguments. On each subsequent cycle variable subtract one hour and thirty minutes.
 ~~~js
-const woo = () => 'woo';
-~~~
-~~~html
-<div>some</div>
-~~~
+const getSleepCycles = (hour, minute) => {
+    const firstCycle = new Date(null, null, null, hour - 9, minute);
+    const secondCycle = new Date(null, null, null, hour - 7, minute - 30);
+    const thirdCycle = new Date(null, null, null, hour - 6, minute);
+    const fourthCycle = new Date(null, null, null, hour - 4, minute - 30);
 
-\`\`\`html
-<div>asdofijsodfij</div>
-\`\`\`
-
-\`\`\`ts
-const pip = (some:string): Person => 'wooo';
-\`\`\`
-
-\`\`\`css
-.class {
-    text-align: center;
+    return [firstCycle, secondCycle, thirdCycle, fourthCycle];
 };
-\`\`\`
+~~~
 `
 
     return (
